2cdebb5b40404f2415f0536335f2dbf089f61c51
Harika <harikakondur3@gmail.com>
2024-12-13 12:20:55 -0700

    demo scene works

--- Assets/scripts/startScene/GameStartManager.cs
+++ Assets/scripts/startScene/GameStartManager.cs
@@ -1,12 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class GameStartManager : MonoBehaviour
 {
-    public void StartGame()
+    private void OnTriggerEnter(Collider other)
+{
+    if (other.CompareTag("nextSceneTile")) 
     {
-        PlayerPrefs.Save();
-        
-        SceneManager.LoadScene("skateparkScene");
+        Debug.Log("Next Scene");
+        SceneManager.LoadScene("Scenes/startScene");
     }
+}
 }
